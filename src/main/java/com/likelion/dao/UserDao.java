28caad32265925ab5be2a3afc1a2a969f98ab882
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-25 14:45:38 +0900

    deleteAll, Add 익명클래스 적용

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -49,7 +49,19 @@ public class UserDao {
     }
 
 
-    public void add(User user) throws SQLException {
+    public void add(final User user) throws SQLException {
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) values (?, ?, ?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                return ps;
+            }
+        });
+
+
         StatementStrategy st = new AddStatement(user);
         jdbcContextWithStatementStrategy(st);
     }
@@ -93,8 +105,12 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                return c.prepareStatement("DELETE FROM users");
+            }
+        });
     }
 
     public int getCount() throws SQLException {
